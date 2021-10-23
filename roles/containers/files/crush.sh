9214af24c4702b845320b387390b503f625a5b1f
Rmaabari <74144712+Rmaabari@users.noreply.github.com>
2021-10-23 23:45:03 +0300

    Create crush.sh

---
+++ roles/containers/files/crush.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+crushtool -c /etc/ceph/de-map -o /etc/ceph/map
+ceph osd setcrushmap -i /etc/ceph/map
+
+#ceph osd getcrushmap -o /etc/ceph/map
+#crushtool -d /etc/ceph/map -o /etc/ceph/de-map
+#sed -i 's/\(.*\)type host/\1type osd/' /etc/ceph/de-map
+#crushtool -c /etc/ceph/de-map -o /etc/ceph/map
+#ceph osd setcrushmap -i /etc/ceph/map
