07361876bdbe1aedc1af17b9d7c2c410d3f5c489
Rmaabari <74144712+Rmaabari@users.noreply.github.com>
2021-12-12 15:40:09 +0200

    Update post.sh

--- roles/containers/files/post.sh
+++ roles/containers/files/post.sh
@@ -3,3 +3,4 @@
 ceph osd rm osd.0
 ceph config set mon auth_allow_insecure_global_id_reclaim false
 radosgw-admin user create --uid=test --display-name=test --access-key=test --secret=test
+echo "log_to_file = True" >> /etc/ceph/ceph.conf
